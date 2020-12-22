0580cb3889580f6d9f54e1d8d6b02fe01c30ee40
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-22 00:24:53 -0700

    Create a transfer struct

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -3,7 +3,14 @@ pragma solidity ^0.6.0;
 contract Wallet {
   address[] public approvers;
   uint public quorum;
-
+  struct Transfer {
+    uint id;
+    uint amount;
+    address payable to;
+    uint approvals;
+    bool sent;
+  }
+  
   constructor(address[] memory _approvers, uint _quorum) public {
     approvers = _approvers;
     quorum    = _quorum;
@@ -12,4 +19,6 @@ contract Wallet {
   function getApprovers() external view returns(address[] memory) {
     return approvers;
   }
+
+
 }
