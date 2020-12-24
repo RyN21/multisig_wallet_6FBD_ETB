63713a77235da2ccb4336211dd5d4840c90eac44
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-23 22:10:40 -0700

    Create test for approvers and quorum

--- test/wallet.js
+++ test/wallet.js
@@ -6,4 +6,16 @@ contract('Wallet', (accounts) => {
     wallet = await Wallet.new([accounts[0], accounts[1], accounts[2]], 2);
     await web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 1000});
   });
+
+  it('Should have correct approvers and quorum', async () => {
+    const approvers = await wallet.getApprovers();
+    const quorum = await wallet.quorum();
+    assert(approvers.length === 3);
+    assert(approvers[0] === accounts[0]);
+    assert(approvers[1] === accounts[1]);
+    assert(approvers[2] === accounts[2]);
+    assert(approvers === [accounts[0], accounts[1], accounts[2]]);
+    assert(quorum === 2);
+
+  });
 });
