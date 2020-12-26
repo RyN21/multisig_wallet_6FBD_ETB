b8ac9412b67935c3bf490fa79b522489bc27afbe
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-26 16:49:37 -0700

    Refactor migration

--- migrations/2_wallet_migration.js
+++ migrations/2_wallet_migration.js
@@ -1,7 +1,8 @@
 const Wallet = artifacts.require('Wallet');
+const approvers = [accounts[0], accounts[1], accounts[2]];
 
 module.exports = async function (deployer, _network, accounts) {
-  await deployer.deploy(Wallet, [accounts[0], accounts[1], accounts[2]], 2);
+  await deployer.deploy(Wallet, approvers, 2);
   const wallet = await Wallet.deployed();
   await web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 10000});
 };
