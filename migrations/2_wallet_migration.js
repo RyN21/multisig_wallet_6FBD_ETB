6943c8cbae8d5130ab736c6b19cffb0f6df69a81
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-26 16:52:07 -0700

    Add notes to migration

--- migrations/2_wallet_migration.js
+++ migrations/2_wallet_migration.js
@@ -3,6 +3,8 @@ const approvers = [accounts[0], accounts[1], accounts[2]];
 
 module.exports = async function (deployer, _network, accounts) {
   await deployer.deploy(Wallet, approvers, 2);
+  // Make a pointer to our deployed smart contract.
   const wallet = await Wallet.deployed();
+  // Send ether to smart contract using web3.js library
   await web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 10000});
 };
