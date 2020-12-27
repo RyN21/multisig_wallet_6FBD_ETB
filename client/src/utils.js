664e9ff48c65bb0274573cb94c9a5cfd6ad5a55c
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-26 23:21:16 -0700

    Build a contract instance: An object produced by web3 that allows you to interact with a smart contract to read and modify data by sending transaction

--- client/src/utils.js
+++ client/src/utils.js
@@ -4,3 +4,14 @@ import Wallet from './contracts/Wallet.sol';
 const getWeb3 = () => {
   return new Web3('http://localhost:9545');
 };
+
+const getWallet = async web3 => {
+  const networkId = await web3.eth.net.getId();
+  const contractDeployment = Wallet.networks[networkId];
+  return new web3.eth.Contract(
+    Wallet.abi,
+    deployedNetwork && deployedNetwork.address
+  );
+};
+
+export ( getWeb3, getWallet )
