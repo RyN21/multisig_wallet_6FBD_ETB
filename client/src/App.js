bbcfed50f177047dc4f4497e01911979d2f93e74
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-27 09:28:38 -0700

    Add variables

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,16 @@ import React, { useEffect, useState } from 'react';
 import { getWeb3, getWallet } from './utils.js';
 
 function App() {
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState(undefined);
+  const [wallet, setWallet] = useState(undefined);
+
+useEffect(() => {
+  const init = async () => {
+    const
+  }
+})
+
   return (
     <div>
       Multisig Dapp
