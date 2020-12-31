a80d1de7694d4dbed10de3666f9a910d96775f4a
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-31 12:28:46 -0700

    Create return html for transfer list function

--- client/src/TransferList.js
+++ client/src/TransferList.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 function TransferList({transfers}) {
-  
+  return (
+    <div>
+    <h2>Transfers</h2>
+      <table>
+
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Amount</th>
+            <th>To</th>
+            <th>approvers</th>
+            <th>sent</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          (trasnfers.map(transfer => (
+            <tr key={transfer.id}>
+              <td>{transfer.id}</td>
+              <td>{transfer.amount}</td>
+              <td>{transfer.to}</td>
+              <td>{transfer.approvers}</td>
+              <td>{transfer.sent ? 'yes' : 'no'}</td>
+          )))
+        </tbody>
+      </table>
+    </div>
   );
 }
 
