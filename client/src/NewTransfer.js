61df93faf5748c4c5e83396ffb89f4cd71a75d56
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-31 12:03:08 -0700

    Create a form to create a new transfer

--- client/src/NewTransfer.js
+++ client/src/NewTransfer.js
@@ -3,6 +3,16 @@ import React, { useState } from 'react';
 function NewTransfer() {
   // define a state for the transfer object that we are going to create with a form
   const [transfer, setTransfer] = useState(undefined);
+
+  // return HTML
+  return (
+    <div>
+      <h2>Create Transfer</h2>
+      <form>
+        
+      </form>
+    </div>
+  )
 }
 
 export default NewTransfer;
