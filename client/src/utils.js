1bd5b4568c0933eee89f41a292f604e7f13c70c1
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-01 22:14:06 -0700

    Add notes to integrating web3 to metamask

--- client/src/utils.js
+++ client/src/utils.js
@@ -4,16 +4,20 @@ import Wallet from './contracts/Wallet.json';
 const getWeb3 = () => {
   return new Promise((resolve, reject) => {
     window.addEventListener('load', async () => {
+      // If metamask is injected in our javascript code
       if(window.ethereum) {
         const web3 = new Web3(window.ethereum); // window.ethereum is a provider object
         try {
+          // 'enable' asks user if you want to grant access to dapp. Privacy reasons
           await window.ethereum.enable();
           resolve(web3);
         } catch(error) {
           reject(error);
         }
+      // If user is using an old metamask Wallet
       } else if (window.web3) {
         resolve(window.web3);
+      // If metamask is not installed
       } else {
         reject('Must install Metamask');
       }
